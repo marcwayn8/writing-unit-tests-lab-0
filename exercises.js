17cdfe8e8d7217f1d0c0abc22159334b833bdac8
marcwayn8 <juniormarch48@gmail.com>
2023-01-19 11:12:36 -0500

    fix:code refactored , lab complete

--- exercises.js
+++ exercises.js
@@ -5,27 +5,24 @@ const isRealPalindrome = str => {
   strs = strs.replace(/ -,'/);
   let newstr = " ";
   for (let i = str.length - 1; i >= 0; i--) {
-      newstr += strs[i];
+    newstr += strs[i];
   }
 
   if (newstr.match(strs) || newstr.match(/[,']/)) {
-      return true;
+    return true;
   }
   return false;
 };
 
-
 // Question 2
-
 
 const runningTotal = arr => {
   let num = 0;
   return arr.map(value => {
-      num += value;
-      return num;
+    num += value;
+    return num;
   });
 };
-
 
 // Question 3
 const swap = string => string.split(' ').map((x, i) => (x.length > 1 ? x[x.length - 1] + x.slice(1, x.length - 1) + x[0] : x)).join(' ');
@@ -38,16 +35,15 @@ const wordSizes = s => {
   return count;
 };
 
-
 // Question 5
 const union = (arr1, arr2) => {
   const nums = [...arr1, ...arr2];
   const newarr = [];
 
   for (let i = 0; i < nums.length; i++) {
-      if (!newarr.includes(nums[i])) {
-          newarr.push(nums[i]);
-      }
+    if (!newarr.includes(nums[i])) {
+      newarr.push(nums[i]);
+    }
   }
 
   return newarr;
@@ -56,11 +52,11 @@ const union = (arr1, arr2) => {
 // Question 6
 const firstRecurring = str => {
   for (let i = 0; i < str.length; i++) {
-      for (let j = i + 1; j < str.length; j++) {
-          if (str[i] === str[j]) {
-              return str[i];
-          }
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return str[i];
       }
+    }
   }
   return '';
 };
@@ -69,7 +65,7 @@ const firstRecurring = str => {
 const showMultiplicativeAverage = nums => {
   let sum = 1;
   for (let i = 0; i < nums.length; i++) {
-      sum *= nums[i];
+    sum *= nums[i];
   }
   return (sum / nums.length).toFixed(3);
 };
@@ -78,19 +74,17 @@ const showMultiplicativeAverage = nums => {
 const multiplyList = (arr1, arr2) => {
   const arr3 = [];
   for (let i = 0; i < arr1.length; i++) {
-      arr3.push(arr1[i] * arr2[i]);
+    arr3.push(arr1[i] * arr2[i]);
   }
 
   return arr3;
 };
 
 const sequence = (n) => {
-  return Array.from({length: n}, (_, i) => i + 1);
+  return Array.from({ length: n }, (_, i) => i + 1);
 };
 
 // The below code connects this JS file to our test file
-
-
 
 module.exports = {
   isRealPalindrome,
